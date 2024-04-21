5bcc53dfbc0e89c9ac01c4de6123cb0e04d56c60
dkshs <88355080+dkshs@users.noreply.github.com>
2024-04-21 01:13:21 -0300

    chore: improve `yml` config

--- src/configs/yml.ts
+++ src/configs/yml.ts
@@ -19,17 +19,38 @@ export async function yml(
 
   return [
     {
+      plugins: {
+        yml: pluginYml,
+      },
+    },
+    {
       files,
       languageOptions: {
         parser: parserYml,
       },
-      plugins: {
-        yml: pluginYml,
-      },
       rules: {
         ...(pluginYml.configs.standard.rules as Rules),
         ...(pluginYml.configs.prettier.rules as Rules),
+        "yml/block-mapping": "error",
+        "yml/block-mapping-question-indicator-newline": "error",
+        "yml/block-sequence": "error",
+        "yml/block-sequence-hyphen-indicator-newline": "error",
+        "yml/flow-mapping-curly-newline": "error",
+        "yml/flow-mapping-curly-spacing": "error",
+        "yml/flow-sequence-bracket-newline": "error",
+        "yml/flow-sequence-bracket-spacing": "error",
+
+        "yml/indent": ["error", 2],
+        "yml/key-spacing": "error",
+        "yml/no-empty-key": "error",
         "yml/no-empty-mapping-value": "off",
+        "yml/no-empty-sequence-entry": "error",
+        "yml/no-irregular-whitespace": "error",
+        "yml/no-tab-indent": "error",
+        "yml/plain-scalar": "error",
+        "yml/quotes": ["error", { avoidEscape: false, prefer: "double" }],
+        "yml/spaced-comment": "error",
+        "yml/vue-custom-block/no-parsing-error": "error",
 
         ...overrides,
       },
