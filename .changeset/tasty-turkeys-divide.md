0faafcbcd30c891e3c1d6c57d92f1000725d24b6
dkshs <88355080+dkshs@users.noreply.github.com>
2024-04-23 16:51:57 -0300

    fix: rule name `react/prop-types`

---
+++ .changeset/tasty-turkeys-divide.md
@@ -0,0 +1,5 @@
+---
+"@dkshs/eslint-config": patch
+---
+
+fix: rule name `react/prop-types`
--- src/configs/react.ts
+++ src/configs/react.ts
@@ -195,7 +195,7 @@ export async function react(
         ...(typescript
           ? {
               "react/jsx-no-undef": "off",
-              "react/prop-type": "off",
+              "react/prop-types": "off",
             }
           : {}),
 
