fd791457a89a6a12ee255a6ea76f1384b81cf0b2
dkshs <88355080+dkshs@users.noreply.github.com>
2024-08-10 12:30:16 -0300

    fix: remove name from `ignores` configuration

---
+++ .changeset/lovely-owls-wash.md
@@ -0,0 +1,5 @@
+---
+"@dkshs/eslint-config": patch
+---
+
+fix: remove name from `ignores` configuration
--- src/configs/ignores.ts
+++ src/configs/ignores.ts
@@ -2,10 +2,5 @@ import type { FlatConfigItem } from "../types";
 import { GLOB_EXCLUDE } from "../globs";
 
 export function ignores(): FlatConfigItem[] {
-  return [
-    {
-      ignores: GLOB_EXCLUDE,
-      name: "dkshs/global-ignores",
-    },
-  ];
+  return [{ ignores: GLOB_EXCLUDE }];
 }
