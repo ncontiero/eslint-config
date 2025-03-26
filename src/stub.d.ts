22a1592108a64181399de8455c28dbc99d7c9129
dkshs <88355080+dkshs@users.noreply.github.com>
2025-03-26 16:59:12 -0300

    chore: remove unused eslint plugin declarations

--- src/stub.d.ts
+++ src/stub.d.ts
@@ -1,8 +1,4 @@
-declare module "eslint-plugin-react";
-declare module "eslint-plugin-react-hooks";
-declare module "eslint-plugin-react-refresh";
 declare module "eslint-plugin-jsx-a11y";
 declare module "@next/eslint-plugin-next";
 declare module "eslint-plugin-promise";
 declare module "eslint-plugin-tailwindcss";
-declare module "@eslint/markdown";
