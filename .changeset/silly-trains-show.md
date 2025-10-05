ec07328ac8303b95e3d5efc103c413e0d9884abd
Nicolas Contiero <git@ncontiero.com>
2025-10-04 22:03:05 -0300

    fix: ensure nextJs option is cast to boolean

---
+++ .changeset/silly-trains-show.md
@@ -0,0 +1,5 @@
+---
+"@ncontiero/eslint-config": patch
+---
+
+fix: ensure nextJs option is cast to boolean
--- src/factory.ts
+++ src/factory.ts
@@ -145,7 +145,7 @@ export function ncontiero(
     javascript({ overrides: getOverrides(options, "javascript") }),
     comments(),
     jsdoc(),
-    imports({ nextJs: enableNextJs === true }),
+    imports({ nextJs: !!enableNextJs }),
     node(),
     promise(),
     command(),
