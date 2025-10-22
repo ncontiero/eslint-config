6894c12930950c8716a906ffb0c49ab835ea17f2
Nicolas Contiero <git@ncontiero.com>
2025-10-22 19:04:36 -0300

    chore(react, next): allow `generateImageMetadata` and `generateSitemaps` in `only-export-components`

---
+++ .changeset/wise-cows-flow.md
@@ -0,0 +1,5 @@
+---
+"@ncontiero/eslint-config": patch
+---
+
+chore(react, next): allow `generateImageMetadata` and `generateSitemaps` in `only-export-components`
--- src/configs/react.ts
+++ src/configs/react.ts
@@ -120,13 +120,18 @@ export async function react(
                     "runtime",
                     "preferredRegion",
                     "maxDuration",
-                    "generateStaticParams",
                     // https://nextjs.org/docs/app/api-reference/functions/generate-metadata
                     "metadata",
                     "generateMetadata",
                     // https://nextjs.org/docs/app/api-reference/functions/generate-viewport
                     "viewport",
                     "generateViewport",
+                    // https://nextjs.org/docs/app/api-reference/functions/generate-image-metadata
+                    "generateImageMetadata",
+                    // https://nextjs.org/docs/app/api-reference/functions/generate-sitemaps
+                    "generateSitemaps",
+                    // https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+                    "generateStaticParams",
                   ]
                 : []),
               ...(isUsingReactRouter
