3fab3d125274f8ce6dd62cc7cad79031fee51c25
Nicolas Contiero <git@ncontiero.com>
2026-04-15 19:04:54 -0300

    fix(node): separate setup from rule configuration

---
+++ .changeset/wicked-symbols-share.md
@@ -0,0 +1,5 @@
+---
+"@ncontiero/eslint-config": patch
+---
+
+fix(node): separate setup from rule configuration
--- src/configs/node.ts
+++ src/configs/node.ts
@@ -1,13 +1,18 @@
 import type { FlatConfigItem } from "../types";
+import { GLOB_SRC } from "../globs";
 import { pluginNode } from "../plugins";
 
 export function node(): FlatConfigItem[] {
   return [
     {
-      name: "ncontiero/node/rules",
+      name: "ncontiero/node/setup",
       plugins: {
         node: pluginNode,
       },
+    },
+    {
+      files: [GLOB_SRC],
+      name: "ncontiero/node/rules",
       rules: {
         "node/handle-callback-err": ["error", "^(err|error)$"],
         "node/no-deprecated-api": "error",
